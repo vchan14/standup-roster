2e9a71869da29090633e6beac1acdfce57bf840c
Visalbotr Chan <chanvisalbotr@gmail.com>
2024-07-22 22:42:53 +1200

    add vue shadcn and tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <script setup lang="ts">
 
+import Button from "@/components/ui/button/Button.vue";
 </script>
 
 <template>
-  <h1>hello world</h1>
+  <div class="app">
+    <h1>hello world</h1>
+    <Button>Click me</Button>
+  </div>
+
 </template>
 
 <style scoped>
+  .app {
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: center;
+    height: 100vh;
+    padding: 10px 5%;
+  }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,24 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss';
+import path from 'node:path'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': path.resolve(__dirname, './src'),
+    },
   }
 })
